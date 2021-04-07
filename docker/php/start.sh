3b5fe70948972e6c2c8753574176e06d3d22d854
Andrey Suchilov <ar2041@bk.ru>
2021-04-07 12:38:30 +0300

    docker compose is updated

---
+++ docker/php/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Waiting for db to be ready..."
+until bin/console dbal:run-sql "SELECT 1" > /dev/null 2>&1; do
+  sleep 1
+done
+
+php bin/console cache:clear --no-warmup
+php bin/console cache:pool:clear cache.app
+php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console cache:warmup
+
+php-fpm
